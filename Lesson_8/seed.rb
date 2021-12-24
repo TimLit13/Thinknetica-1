6a62248d74b433a608c6fef7145d633951026d10
“TimLit13” <litidm@gmail.com>
2021-12-24 21:46:36 +0300

    Fix methods length with rubocop recommendations

--- Lesson_8/seed.rb
+++ Lesson_8/seed.rb
@@ -1,37 +1,56 @@
 module Seed
   def seed
-    station_1 = Station.new('Spb')
-    @stations.push(station_1)
-    station_2 = Station.new('Viborg')
-    @stations.push(station_2)
-    station_3 = Station.new('Lahd')
-    @stations.push(station_3)
-    station_4 = Station.new('Sortavala')
-    @stations.push(station_4)
 
-    route_1 = Route.new(station_1, station_4)
-    @routes.push(route_1)
-    route_1.add_station(station_2)
-    route_1.add_station(station_3)
+    add_seed_stations
 
-    route_2 = Route.new(station_4, station_1)
-    @routes.push(route_2)
-    route_2.add_station(station_2)
+    add_seed_routes
 
-    train_1 = PassengerTrain.new('111-11')
-    train_2 = CargoTrain.new('222-22')
-    @trains.push(train_1)
-    @trains.push(train_2)
+    add_seed_trains
 
+    add_seed_carriages
+
+    @train1.make_route(@route1)
+    @train2.make_route(@route2)
+  end
+
+  private
+
+  def add_seed_stations
+    @station1 = Station.new('Spb')
+    @stations.push(@station1)
+    @station2 = Station.new('Viborg')
+    @stations.push(@station2)
+    @station3 = Station.new('Lahd')
+    @stations.push(@station3)
+    @station4 = Station.new('Sortavala')
+    @stations.push(@station4)
+  end
+
+  def add_seed_routes
+    @route1 = Route.new(@station1, @station4)
+    @routes.push(@route1)
+    @route1.add_station(@station2)
+    @route1.add_station(@station3)
+
+    @route2 = Route.new(@station4, @station1)
+    @routes.push(@route2)
+    @route2.add_station(@station2)
+  end
+
+  def add_seed_trains
+    @train1 = PassengerTrain.new('111-11')
+    @train2 = CargoTrain.new('222-22')
+    @trains.push(@train1)
+    @trains.push(@train2)
+  end
+
+  def add_seed_carriages
     5.times do
-      train_1.add_carriage(PassengerCarriage.new(rand(36..54)))
+      @train1.add_carriage(PassengerCarriage.new(rand(36..54)))
     end
 
     50.times do
-      train_2.add_carriage(CargoCarriage.new(rand(75.0..158.0)))
+      @train2.add_carriage(CargoCarriage.new(rand(75.0..158.0)))
     end
-
-    train_1.set_route(route_1)
-    train_2.set_route(route_2)
   end
 end
